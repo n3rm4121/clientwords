e36df01f2c44b32f04901aae3581741be7b4c0e9
n3rm4121 <nirmaldhakal2032@gmail.com>
2024-09-08 21:51:08 +0545

    Add @vercel/speed-insights package to package.json and include SpeedInsights component in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./styles/globals.css"
 import type { Metadata } from "next";
 import { cn } from '../lib/utils'
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -41,6 +42,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </SessionProvider>
         </ThemeProvider>
         <Analytics/>
+        <SpeedInsights/>
           
       </body>
     </html>
