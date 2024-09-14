0160b9869d521fccef08e921245b502cce574e6b
n3rm4121 <nirmaldhakal2032@gmail.com>
2024-09-14 19:04:55 +0545

    Refactor middleware to handle authentication and redirection
    - Update middleware to redirect to the login page if the user is not authenticated and trying to access the dashboard
    - Update middleware to redirect to the dashboard if the user is authenticated and trying to access the login page
    - Remove commented out code related to Content-Security-Policy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,29 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export default function middleware(req: NextRequest) {
-  const token = req.cookies.get('authjs.session-token'); 
+  const token = req.cookies.get('authjs.session-token');
   const url = req.nextUrl.clone();
-  if (!token) {
-    if (url.pathname.startsWith('/dashboard')) {
-      return NextResponse.redirect(new URL('/login', req.url));
-    }
-  } else {  
-    if (url.pathname.startsWith('/login')) {
-      return NextResponse.redirect(new URL('/dashboard', req.url));
-    }
+
+  // If user is not authenticated and trying to access dashboard
+  if (!token && url.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/login', req.url));
   }
-  // res.headers.set(
-  //   'Content-Security-Policy',
-  //   "frame-ancestors 'self' https://sandbox-buy.paddle.com/ https://checkout.paddle.com/;"
-  // );
+
+  // If user is authenticated and trying to access login page
+  if (token && url.pathname.startsWith('/login')) {
+    return NextResponse.redirect(new URL('/dashboard', req.url));
+  }
 
   return NextResponse.next();
-
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/login/:path*'], // Adjust based on your routes
+  matcher: ['/dashboard/:path*', '/login'], // Adjust based on your routes
 };
+
+
+
+ // res.headers.set(
+  //   'Content-Security-Policy',
+  //   "frame-ancestors 'self' https://sandbox-buy.paddle.com/ https://checkout.paddle.com/;"
+  // );
