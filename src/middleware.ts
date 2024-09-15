7f64a0583d86c1eb06263e189b097ad925eb59a6
n3rm4121 <nirmaldhakal2032@gmail.com>
2024-09-15 09:28:00 +0545

    Refactor middleware to use relative image path and add remotePatterns for image domains

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,46 +1,12 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { getToken } from 'next-auth/jwt';
-
-const privatePaths = ['/dashboard', '/dashboard/:path*'];
-
-export async function middleware(request: NextRequest) {
-  const secret = process.env.AUTH_SECRET;
-  
-  if (!secret) {
-    throw new Error('Please provide a secret');
+import { auth } from "@/auth"
+ 
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname !== "/login") {
+    const newUrl = new URL("/login", req.nextUrl.origin)
+    return Response.redirect(newUrl)
   }
-  
-  // Retrieve and verify the token from the request
-  const token = await getToken({
-    req: request,
-    secret: secret,
-    raw: true,
-    salt: ''
-  });
+})
 
-  const isPrivatePath = privatePaths.some(path => 
-    request.nextUrl.pathname.startsWith(path.replace(':path*', ''))
-  );
-
-  // Redirect unauthenticated users from private paths to login
-  if (!token && isPrivatePath) {
-    const url = new URL('/login', request.nextUrl.origin);
-    url.searchParams.set('callbackUrl', request.nextUrl.pathname);
-    console.log(url);
-    return NextResponse.redirect(url);
-  }
-
-  // Redirect authenticated users from login page to dashboard
-  if (token && request.nextUrl.pathname === '/login') {
-    const url = new URL('/dashboard', request.nextUrl.origin);
-    return NextResponse.redirect(url);
-  }
-
-  return NextResponse.next();
-}
-
-// Configure middleware to run on specific paths
 export const config = {
-  matcher: ["/dashboard/:path*", '/login'],
-};
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
