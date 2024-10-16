dab161ce24edccc0b7add4e22151d3f470cbe1f1
n3rm4121 <nirmaldhakal2032@gmail.com>
2024-10-16 13:01:52 +0545

    Refactor UI styles in ThankYou component

--- src/components/dashboard/Testimonial/ThankYou.tsx
+++ src/components/dashboard/Testimonial/ThankYou.tsx
@@ -18,67 +18,65 @@ export function Thankyou({ userName, companyName, companyURL }: { userName: stri
 
   return (
 
-     <MaxWidthWrapper className='bg-gradient-to-r from-blue-500 to-purple-500'>
-    
-            {/* Fixed Header */}
-            <div className="fixed p-6 top-0 left-0 w-full z-10 ">
-        <div className="relative inline-flex items-center">
-                <Link href="/" className="flex-shrink-0">
-                  <Image src='/brand.png' width={200} height={200} alt='ClientWords' />
-                </Link>
-                {/* <Badge variant={'secondary'} className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2">
-                  Beta
-                </Badge> */}
-              </div>
-        </div>
-    
-    
-    <div className="min-h-screen flex items-center justify-center px-4">
-      <motion.div 
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1 }}
-        className="bg-white bg-opacity-80 rounded-3xl shadow-2xl p-8 max-w-lg w-full text-center"
-      >
-        <motion.div
-          initial={{ scale: 0 }}
-          animate={{ scale: 1 }}
-          transition={{ delay: 0.3, type: 'spring', stiffness: 120 }}
-        >
-          <svg className="w-28 h-28 mx-auto text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-          </svg>
-        </motion.div>
-        
-        <motion.h1 
-          className="text-4xl font-bold mt-8 mb-4 text-gray-900"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.5 }}
-        >
-          Thank You, {userName}!
-        </motion.h1>
-        
-        <motion.p 
-          className="text-gray-700 mb-10 leading-relaxed tracking-tight"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.7 }}
-        >
-          Your testimonial has been successfully submitted. <br /> We appreciate your valuable feedback!
-        </motion.p>
-        
-        <motion.button
-          className="rounded-3xl bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold py-2 px-6 hover:shadow-lg transition duration-300 ease-in-out transform  outline-dashed"
-          whileHover={{ scale: 1.08 }}
-          whileTap={{ scale: 0.95 }}
-          onClick={() => window.open(companyURL, '_blank')}
+    <MaxWidthWrapper className='bg-gradient-to-r from-blue-500 to-purple-500'>
 
+      {/* Fixed Header */}
+      <div className="fixed p-6 top-0 left-0 w-full z-10 ">
+        <div className="relative inline-flex items-center">
+          <Link href="/" className="flex-shrink-0">
+            <Image src='/newbrand1.png' width={200} height={200} alt='ClientWords' />
+          </Link>
+
+        </div>
+      </div>
+
+
+      <div className="min-h-screen flex items-center justify-center px-4">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          className="bg-white bg-opacity-80 rounded-3xl shadow-2xl p-8 max-w-lg w-full text-center"
         >
-          Visit {companyName}
-        </motion.button>
-      </motion.div>
-    </div>
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ delay: 0.3, type: 'spring', stiffness: 120 }}
+          >
+            <svg className="w-28 h-28 mx-auto text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+          </motion.div>
+
+          <motion.h1
+            className="text-4xl font-bold mt-8 mb-4 text-gray-900"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5 }}
+          >
+            Thank You, {userName}!
+          </motion.h1>
+
+          <motion.p
+            className="text-gray-700 mb-10 leading-relaxed tracking-tight"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.7 }}
+          >
+            Your testimonial has been successfully submitted. <br /> We appreciate your valuable feedback!
+          </motion.p>
+
+          <motion.button
+            className="rounded-3xl bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold py-2 px-6 hover:shadow-lg transition duration-300 ease-in-out transform  outline-dashed"
+            whileHover={{ scale: 1.08 }}
+            whileTap={{ scale: 0.95 }}
+            onClick={() => window.open(companyURL, '_blank')}
+
+          >
+            Visit {companyName}
+          </motion.button>
+        </motion.div>
+      </div>
     </MaxWidthWrapper>
   );
 }
--- src/components/dashboard/Testimonial/testimonailSubmit.tsx
+++ src/components/dashboard/Testimonial/testimonailSubmit.tsx
@@ -3,7 +3,6 @@ import React, { useState, useRef, useEffect } from "react";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { ITestimonialCard } from "@/models/testimonial-card.model";
 import { Input } from "@/components/ui/input";
 import { z } from 'zod';
 import Link from "next/link";
@@ -14,7 +13,6 @@ import 'react-toastify/dist/ReactToastify.css';
 import { FcAddImage } from "react-icons/fc";
 import { Thankyou } from "./ThankYou";
 import Image from "next/image";
-import { Badge } from "@/components/ui/badge";
 
 const TestimonialSubmit = ({ testimonialCardData }: { testimonialCardData: any }) => {
     const [userName, setUsername] = useState<string>('');
@@ -26,7 +24,7 @@ const TestimonialSubmit = ({ testimonialCardData }: { testimonialCardData: any }
     const fileInputRef = useRef<HTMLInputElement | null>(null);
     const [showThankyou, setShowThankyou] = useState(false);
     const [thankyouName, setThankyouName] = useState<string>('');
- 
+
 
     const handleAvatarClick = () => {
         if (fileInputRef.current) {
@@ -98,13 +96,13 @@ const TestimonialSubmit = ({ testimonialCardData }: { testimonialCardData: any }
             newForm.append('userAvatar', data.userAvatar!); // `!` asserts that it's not null
             newForm.append('spaceId', data.spaceId);
             newForm.append('recaptchaToken', token);
-            const res = await axios.post('/api/testimonial', newForm,{
+            const res = await axios.post('/api/testimonial', newForm, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
-                }, 
-                
+                },
+
             });
-               if(res.status === 200){
+            if (res.status === 200) {
                 setThankyouName(userName.split(' ')[0]);
                 setUsername('');
                 setUserIntro('');
@@ -113,8 +111,8 @@ const TestimonialSubmit = ({ testimonialCardData }: { testimonialCardData: any }
                 setUserAvatarPreview('');
                 setShowThankyou(true);
 
-               }
-                
+            }
+
 
         } catch (error) {
             if (error instanceof z.ZodError) {
@@ -131,93 +129,90 @@ const TestimonialSubmit = ({ testimonialCardData }: { testimonialCardData: any }
 
     if (showThankyou) {
 
-        return <Thankyou userName={thankyouName} companyURL={testimonialCardData.companyURL}  companyName={testimonialCardData.companyName} />;
+        return <Thankyou userName={thankyouName} companyURL={testimonialCardData.companyURL} companyName={testimonialCardData.companyName} />;
     }
     return (
-      <div className="bg-gradient-to-r from-blue-500 to-purple-500 shadow-lg">
-        
-        {/* Fixed Header */}
-        <div className="fixed p-6 top-0 left-0 w-full z-10 ">
-        <div className="relative inline-flex items-center">
-                <Link href="/" className="flex-shrink-0">
-                  <Image src='/brand.png' width={200} height={200} alt='ClientWords' />
-                </Link>
-                {/* <Badge variant={'secondary'} className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2">
-                  Beta
-                </Badge> */}
-              </div>
-        </div>
-    
-        {/* Main Content */}
-        <form onSubmit={handleFormSubmit} className="p-6 relative flex flex-col items-center justify-center min-h-screen max-h-screen overflow-y-auto bg-gradient-to-br from-blue-50 to-blue-100">
-            {/* Abstract Light Effect Background */}
-            <div className="absolute inset-0 bg-gradient-to-br from-purple-300 via-transparent to-blue-300 opacity-40 blur-3xl" />
-            
-            <div className="relative z-10 flex items-center justify-center gap-4 mb-4">
-                <Avatar className="h-16 w-16">
-                    <AvatarImage
-                        src={testimonialCardData.companyLogo ? testimonialCardData.companyLogo : '/user.png'}
-                        alt="companyLogo"
-                    />
-                    <AvatarFallback><AvatarImage src={"/user.png"} /></AvatarFallback>
-                </Avatar>
-                <h2
-                    onClick={() => testimonialCardData.companyURL && window.open(testimonialCardData.companyURL, '_blank')}
-                    className="text-xl font-bold text-blue-600 cursor-pointer hover:underline"
-                >
-                    {testimonialCardData.companyName}
-                </h2>
+        <div className="bg-gradient-to-r from-blue-500 to-purple-500 shadow-lg">
+
+            {/* Fixed Header */}
+            <div className="fixed p-6 top-0 left-0 w-full z-10 ">
+                <div className="relative inline-flex items-center">
+                    <Link href="/" className="flex-shrink-0">
+                        <Image src='/newbrand1.png' width={200} height={200} alt='ClientWords' />
+                    </Link>
+                </div>
             </div>
-    
-            <div className="relative p-6 rounded-3xl flex flex-col shadow-xl border-2 border-blue-500  max-w-md mx-auto bg-white bg-opacity-90">
-                <div className="flex gap-4 text-center">
-                    <Avatar className="h-16 w-16 cursor-pointer" onClick={handleAvatarClick}>
-                        <AvatarImage src={userAvatarPreview} alt="userAvatar" />
-                        <AvatarFallback><FcAddImage size={50} /></AvatarFallback>
+
+            {/* Main Content */}
+            <form onSubmit={handleFormSubmit} className="p-6 relative flex flex-col items-center justify-center min-h-screen max-h-screen overflow-y-auto bg-gradient-to-br from-blue-50 to-blue-100">
+                {/* Abstract Light Effect Background */}
+                <div className="absolute inset-0 bg-gradient-to-br from-purple-300 via-transparent to-blue-300 opacity-40 blur-3xl" />
+
+                <div className="relative z-10 flex items-center justify-center gap-4 mb-4">
+                    <Avatar className="h-16 w-16">
+                        <AvatarImage
+                            src={testimonialCardData.companyLogo ? testimonialCardData.companyLogo : '/user.png'}
+                            alt="companyLogo"
+                        />
+                        <AvatarFallback><AvatarImage src={"/user.png"} /></AvatarFallback>
                     </Avatar>
-                    <Input
-                        type="file"
-                        ref={fileInputRef}
-                        className="hidden"
-                        accept="image/*"
-                        onChange={handleFileChange}
-                    />
-                    <div>
-                        <h3 className="text-2xl font-semibold text-gray-700">
+                    <h2
+                        onClick={() => testimonialCardData.companyURL && window.open(testimonialCardData.companyURL, '_blank')}
+                        className="text-xl font-bold text-blue-600 cursor-pointer hover:underline"
+                    >
+                        {testimonialCardData.companyName}
+                    </h2>
+                </div>
+
+                <div className="relative p-6 rounded-3xl flex flex-col shadow-xl border-2 border-blue-500  max-w-md mx-auto bg-white bg-opacity-90">
+                    <div className="flex gap-4 text-center">
+                        <Avatar className="h-16 w-16 cursor-pointer" onClick={handleAvatarClick}>
+                            <AvatarImage src={userAvatarPreview} alt="userAvatar" />
+                            <AvatarFallback><FcAddImage size={50} /></AvatarFallback>
+                        </Avatar>
+                        <Input
+                            type="file"
+                            ref={fileInputRef}
+                            className="hidden"
+                            accept="image/*"
+                            onChange={handleFileChange}
+                        />
+                        <div>
+                            <h3 className="text-2xl font-semibold text-gray-700">
+                                <input
+                                    type="text"
+                                    className="border-none bg-transparent outline-none p-0 m-0 w-full"
+                                    placeholder="John Doe"
+                                    value={userName}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                            </h3>
                             <input
                                 type="text"
-                                className="border-none bg-transparent outline-none p-0 m-0 w-full"
-                                placeholder="John Doe"
-                                value={userName}
-                                onChange={(e) => setUsername(e.target.value)}
+                                className="border-none text-gray-500 bg-transparent outline-none p-0 m-0 w-full"
+                                placeholder="CEO at XYZ (optional)"
+                                value={userIntro}
+                                onChange={(e) => setUserIntro(e.target.value)}
                             />
-                        </h3>
-                        <input
-                            type="text"
-                            className="border-none text-gray-500 bg-transparent outline-none p-0 m-0 w-full"
-                            placeholder="CEO at XYZ (optional)"
-                            value={userIntro}
-                            onChange={(e) => setUserIntro(e.target.value)}
-                        />
+                        </div>
                     </div>
+
+                    <p className="text-gray-700 font-semibold my-4 text-center">{testimonialCardData.promptText}</p>
+
+                    <Textarea
+                        placeholder={testimonialCardData.placeholder}
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        className="resize-none h-32 w-full px-4 py-2 rounded-2xl border border-blue-400 text-black mb-4"
+                    />
+                    <Button className="rounded-3xl bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold py-2 px-6 hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1" type="submit" loading={loading} disabled={loading}>
+                        {loading ? 'Submitting...' : 'Submit'}
+                    </Button>
                 </div>
-    
-                <p className="text-gray-700 font-semibold my-4 text-center">{testimonialCardData.promptText}</p>
-    
-                <Textarea
-                    placeholder={testimonialCardData.placeholder}
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    className="resize-none h-32 w-full px-4 py-2 rounded-2xl border border-blue-400 text-black mb-4"
-                />
-                <Button className="rounded-3xl bg-gradient-to-r from-blue-500 to-purple-600 text-white font-semibold py-2 px-6 hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1" type="submit" loading={loading} disabled={loading}>
-                    {loading ? 'Submitting...' : 'Submit'}
-                </Button>
-            </div>
-        </form>
-    
-    </div>
-    
+            </form>
+
+        </div>
+
     );
 };
 
